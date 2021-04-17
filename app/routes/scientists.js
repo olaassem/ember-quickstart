d9d275bbefb413f52f690c8b4eb8a01c25e27fc8
Ola Assem <ola.assem@viget.com>
2021-04-17 06:32:15 +0200

    scientists data model & renders scientist with each

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class ScientistsRoute extends Route {
+  //In a route's model() method, you return whatever data you want to make available to the template.
+  model() {
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
 }
