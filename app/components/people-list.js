c812a4f96f7c959d38b35c69835c517ad6fa05ee
Ola Assem <ola.assem@viget.com>
2021-04-17 20:15:03 +0200

    click action

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class PeopleListComponent extends Component {
+  // The @action decorator indicates we want to use this method as an action in our template, in response to user interaction.
   @action
   showPerson(person) {
     alert(`The person's name is ${person}!`);
